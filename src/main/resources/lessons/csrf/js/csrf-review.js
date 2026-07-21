91aeaf7a14c94ff6bfa881c2949675bf045af771
Willie <willie_kao@gss.com.tw>
2026-07-21 13:54:43 +0800

    init

---
+++ src/main/resources/lessons/csrf/js/csrf-review.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+//    $("#postReview").on("click", function () {
+//        var commentInput = $("#reviewInput").val();
+//        $.ajax({
+//            type: 'POST',
+//            url: 'csrf/review',
+//            data: JSON.stringify({text: commentInput}),
+//            contentType: "application/json",
+//            dataType: 'json'
+//        }).then(
+//            function () {
+//                getChallenges();
+//                $("#commentInput").val('');
+//            }
+//        )
+//    });
+
+    var html = '<li class="comment">' +
+        '<div class="pull-left">' +
+        '<img class="avatar" src="images/avatar1.png" alt="avatar"/>' +
+        '</div>' +
+        '<div class="comment-body">' +
+        '<div class="comment-heading">' +
+        '<h4 class="user">USER / STARS stars</h4>' +
+        '<h5 class="time">DATETIME</h5>' +
+        '</div>' +
+        '<p>COMMENT</p>' +
+        '</div>' +
+        '</li>';
+
+    getChallenges();
+
+    function getChallenges() {
+        $("#list").empty();
+        // $.get('csrf/review', function (result, status) {
+        //     for (var i = 0; i < result.length; i++) {
+        //         var comment = html.replace('USER', result[i].user);
+        //         comment = comment.replace('DATETIME', result[i].dateTime);
+        //         comment = comment.replace('COMMENT', result[i].text);
+        //         comment = comment.replace('STARS', result[i].stars)
+        //         $("#list").append(comment);
+        //     }
+
+        // });
+        $.get('csrf/review', function (result, status) {
+            for (var i = 0; i < result.length; i++) {
+                var comment = html.replace('USER', escapeHtml(result[i].user));
+                comment = comment.replace('DATETIME', escapeHtml(result[i].dateTime));
+                comment = comment.replace('COMMENT', escapeHtml(result[i].text));   // use escapeHtml function to neutralize potential scripts
+                comment = comment.replace('STARS', escapeHtml(result[i].stars));    // use escapeHtml function to neutralize potential scripts
+                $("#list").append(comment);
+            }
+        });
+    }
+
+    function escapeHtml(unsafe) {
+        return unsafe
+             .replace(/&/g, "&amp;")
+             .replace(/</g, "&lt;")
+             .replace(/>/g, "&gt;")
+             .replace(/"/g, "&quot;")
+             .replace(/'/g, "&#039;");
+    }
+})
---
+++ src/main/resources/lessons/jwt/js/jwt-final.js
@@ -0,0 +1,8 @@
+function follow(user) {
+    $.ajax({
+        type: 'POST',
+        url: 'JWT/final/follow/' + user
+    }).then(function (result) {
+        $("#toast").append(result);
+    })
+}
